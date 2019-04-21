3a0e60e6e3009a0a2b33dbe0c6408b40ae25a5db
Saurav Dhungana <saurav.dh@gmail.com>
2019-04-21 13:12:12 +0545

    initial commit

---
+++ scrape_twitter.py
@@ -0,0 +1,45 @@
+import tweepy
+import csv
+import pandas as pd
+
+## Input your credentials here
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+##  Game of thrones
+search_query = "#got OR #gameofthrones OR #got8" + " -filter:retweets"
+
+# Open/Create a file to append data
+csvFile = open('got_new.csv', 'a')
+
+# Use csv Writer
+csvWriter = csv.writer(csvFile)
+# csvWriter.writerow(['created_at', 'tweet_id', 'user', 'location', 'text', 'likes', 'retweets'])
+
+
+## Search query API Documentation
+## https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets.html
+
+tweet_count = 0 
+for tweet in tweepy.Cursor(api.search,
+                           q = search_query, 
+                           count = 100,
+                           tweet_mode='extended',
+                           lang = "en",
+                           since = "2019-04-14",
+                           until = "2019-04-16"
+                        ).items():
+    tweet_count += 1
+    if tweet_count % 1000 == 0:
+       print(f'{tweet_count} tweets downloaded')
+
+    if tweet.full_text:
+      csvWriter.writerow([tweet.created_at, tweet.id, tweet.user.screen_name, 
+         tweet.user.location, tweet.full_text, 
+         tweet.favorite_count, tweet.retweet_count])
